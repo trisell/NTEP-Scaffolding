1d7dcd17fdc21f50a51b12a01965e29a7564b54e
Dominic Desimini <ddesimini@fortressinfosec.com>
2019-04-24 09:34:55 -0600

    Setup base router, pg models, and logging.

---
+++ db.sql
@@ -0,0 +1,30 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.2
+-- Dumped by pg_dump version 11.1
+
+-- Started on 2019-04-10 12:57:42 MDT
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 7 (class 2615 OID 16385)
+-- Name: html; Type: SCHEMA; Schema: -; Owner: htmlAdmin
+--
+
+CREATE SCHEMA application;
+
+DROP SCHEMA public;
+
+ALTER SCHEMA application OWNER TO "Admin";
+
